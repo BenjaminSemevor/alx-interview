eb83b9eda549ee3fc60e2fc2197d2ddc40cbc8a0
BenjaminSemevor <benjaminsemevor@ymail.com>
2024-05-14 23:25:11 +0000

    Second commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-Reads stdin line by line and computes metrics.
-"""
+"""reads stdin line by line and computes metrics:"""
 
 import sys
-from collections import defaultdict
 
-cache = defaultdict(int)
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
 counter = 0
 
@@ -16,23 +14,23 @@ try:
         if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
-            if code in cache:
+            if code in cache.keys():
                 cache[code] += 1
             total_size += size
             counter += 1
 
         if counter == 10:
             counter = 0
-            print('File size:', total_size)
+            print('File size: {}'.format(total_size))
             for key, value in sorted(cache.items()):
                 if value != 0:
-                    print(f'{key}: {value}')
+                    print('{}: {}'.format(key, value))
 
-except KeyboardInterrupt:
+except Exception as err:
     pass
 
 finally:
-    print('File size:', total_size)
+    print('File size: {}'.format(total_size))
     for key, value in sorted(cache.items()):
         if value != 0:
-            print(f'{key}: {value}')
+            print('{}: {}'.format(key, value))
